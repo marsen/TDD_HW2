7354427337e2439c2b2b9fcf1b5d304ffd2e4067
marklin <marklin@nine-yi.com>
2016-03-15 17:37:55 +0800

    TDD HW2 - 完成Production， Scenario: 一二三集各買了一本，價格應為100*3*0.9=270

--- MallLib/PotterShoppingCart.cs
+++ MallLib/PotterShoppingCart.cs
@@ -12,12 +12,12 @@ namespace MallLib
 
         public decimal Check(List<PotterBook> books)
         {
-            decimal totalPrice;
+            decimal totalPrice = 0;
             if (books.Count == 1)
             {
                 totalPrice = books.Sum(x => x.Price);                
             }
-            else
+            else if (books.Count == 2)
             {
                 if (books.ElementAt(0).Volume == books.ElementAt(1).Volume)
                 {
@@ -26,6 +26,35 @@ namespace MallLib
                 else
                 {
                     totalPrice = 0.95m * books.Sum(x => x.Price);
+                }
+            }
+            else
+            {
+                var groupedBookList = books.GroupBy(x => x.Volume);
+                var groupCount = groupedBookList.Count();
+                if (groupCount == 1)
+                {
+                    totalPrice = books.Sum(x => x.Price);
+                }
+                else if (groupCount == 3)
+                {
+                    totalPrice = 0.9m * books.Sum(x => x.Price);
+                }
+                else
+                {
+                    //groupCount == 2
+                    for (int i = 0; i < groupCount; i++)
+                    {
+                        var groupedBooks = groupedBookList.ElementAt(i);
+                        if (groupedBooks.Count() == 1)
+                        {
+                            totalPrice += groupedBooks.Sum(x => x.Price);
+                        }
+                        else
+                        {
+                            totalPrice += 0.95m * groupedBooks.Sum(x => x.Price);
+                        }
+                    }
                 }
             }
             return totalPrice;
