a3e6071231634744fc981fec4edf5ae8717461af
marklin <marklin@nine-yi.com>
2016-03-17 00:22:49 +0800

    TDD HW2 - Run StyleCop

--- MallLib/PotterShoppingCart.cs
+++ MallLib/PotterShoppingCart.cs
@@ -1,26 +1,31 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
 
 namespace MallLib
 {
+    /// <summary>
+    /// PotterShoppingCart
+    /// </summary>
     public class PotterShoppingCart
     {
+        /// <summary>
+        /// 折扣表
+        /// </summary>
         private Dictionary<int, decimal> discountLogic =
-            new Dictionary<int, decimal>()
-            {
-                { 1 , 1.00m },
-                { 2 , 0.95m },
-                { 3 , 0.90m },
-                { 4 , 0.80m },
-                { 5 , 0.75m }
+            new Dictionary<int, decimal>() { { 1, 1.00m }, { 2, 0.95m }, { 3, 0.90m }, { 4, 0.80m }, { 5, 0.75m } };
 
-            };
-
+        /// <summary>
+        /// Initializes a new instance of the <see cref="PotterShoppingCart" /> class.
+        /// </summary>
         public PotterShoppingCart()
         {
         }
 
+        /// <summary>
+        /// Check
+        /// </summary>
+        /// <param name="books">book list in cart</param>
+        /// <returns>best price</returns>
         public decimal Check(List<PotterBook> books)
         {
             books = books.OrderBy(x => x.Volume).ToList();
@@ -43,30 +48,37 @@ namespace MallLib
                     else
                     {
                         template[pointer].Add(book);
-                    }                    
+                    }
                 }
                 else
                 {
                     pointer = 0;
                     template[pointer].Add(book);
-   
                 }
+
                 lastbook = book;
             }
+
             decimal result = 0m;
             foreach (var groupBookList in template)
             {
                 if (groupBookList != null)
                 {
-                    result += GetDiscount(groupBookList) * groupBookList.Sum(x => x.Price);
+                    result += this.GetDiscount(groupBookList) * groupBookList.Sum(x => x.Price);
                 }
             }
+
             return result;
         }
 
+        /// <summary>
+        /// GetDiscount
+        /// </summary>
+        /// <param name="bookList">book</param>
+        /// <returns>price after discount</returns>
         private decimal GetDiscount(List<PotterBook> bookList)
         {
-            return discountLogic[bookList.Count()];
+            return this.discountLogic[bookList.Count()];
         }
     }
 }
